b0d9a5ff50abe9be5fbcec1f5b7a014172cdac11
oyeeroyalteam91 <royalnobi768@gmail.com>
2026-06-02 10:15:00 +0530

    Use sticker replies in chat

--- handlers/chat.py
+++ handlers/chat.py
@@ -4,6 +4,7 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 from config import settings
+from handlers.stickers import maybe_reply_with_sticker
 from utils.stylish_text import s
 
 
@@ -20,6 +21,9 @@ async def chat_reply(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
         return
 
     if message.chat.type in {"group", "supergroup"} and not should_reply(message.text):
+        return
+
+    if await maybe_reply_with_sticker(update, context):
         return
 
     if user.id == settings.aliza_id:
